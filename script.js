1e0c738239daa179a674ad562938da66f8c52556
Margaret-wanjiru <maggiesamwells>
2025-05-01 20:54:13 +0300

    change

--- script.js
+++ script.js
@@ -9,5 +9,24 @@ const places=[
   new place('New York', 'Statue of Liberty, Central Park', 'Fall', 'Beautiful fall colors, busy city life'),
   new place('Tokyo', 'Shibuya Crossing, Tokyo Tower', 'Winter', 'Cold but festive vibes, lots of shopping'),
 ]
+const placeList=document.getElementById("placeList")
+places.forEach((place,index)=>{
+  const placeDiv = document.createElement("div")
+  placeDiv.className="place"
+  placeDiv.innerText= place.location
+  placeDiv.addEventListener('click',()=>{
+    const display=document.getElementById("formSection-${index}")
+  })
+  const detailsDiv=document.createElement('div')
+  detailsDiv.className = 'details';
+            detailsDiv.id = `details-${index}`;
+            detailsDiv.innerHTML = `
+                <strong>Landmarks:</strong> ${place.landmarks}<br>
+                <strong>Time of Year:</strong> ${place.timeOfYear}<br>
+                <strong>Notes:</strong> ${place.notes}
+            `
+            placeDiv.appendChild(detailsDiv)
+            placeList.appendChild(placeDiv)
 
+})
   
