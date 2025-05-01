22027b824d5f12965663e3b032a2b6acc3ee847a
Margaret-wanjiru <maggiesamwells>
2025-05-01 20:32:36 +0300

    add

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+function place(location,landmarks,timeOfYear,notes){
+  this.location = location
+  this.landmarks = landmarks
+  this.timeOfYear=timeOfYear
+  this.notes=notes
+}
+const places=[
+  new place("new Place('Paris', 'Eiffel Tower, Louvre Museum', 'Spring', 'Great weather, amazing food!'"),
+  new place('New York', 'Statue of Liberty, Central Park', 'Fall', 'Beautiful fall colors, busy city life'),
+  new place('Tokyo', 'Shibuya Crossing, Tokyo Tower', 'Winter', 'Cold but festive vibes, lots of shopping'),
+]
 
   
